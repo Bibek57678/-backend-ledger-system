2b12a2a2c1431bba96e6181a5093dba4996c487f
Biiibek <biiibekbarman@gmail.com>
2026-07-20 05:00:56 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,16 +4,11 @@ const authController = require("../controllers/auth.controller")
 const router = express.Router()
 
 
-/* POST /api/auth/register */
 router.post("/register", authController.userRegisterController)
 
 
-/* POST /api/auth/login */
 router.post("/login",authController.userLoginController)
 
-/**
- * - POST /api/auth/logout
- */
 router.post("/logout", authController.userLogoutController)
 
 
